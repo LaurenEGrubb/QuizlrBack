67070cf0d5e0da32fd2bd62a1caba368138ca107
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-09-09 11:11:35 -0400

    successfully made a delete route for the flashcards

--- routes/FlashcardRouter.js
+++ routes/FlashcardRouter.js
@@ -6,6 +6,6 @@ const controller = require('../controllers/FlashcardController')
 Router.get('/:flashcard_id/get', controller.GetFlashcardDetails)
 Router.post('/:set_id/create', controller.CreateFlashcard)
 Router.put('/:set_id/create/:flashcard_id/update', controller.UpdateFlashcard)
-// Router.delete('/:set_id/:flashcard_id/delete', controller.DeleteFlashcard)
+Router.delete('/:set_id/create/:flashcard_id/delete', controller.DeleteFlashcard)
 // Implement these routes
 module.exports = Router
