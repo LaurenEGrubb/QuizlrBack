b6409279e2a8fce5799aa486d7dc9648d9da93c9
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-09-12 17:34:22 -0400

    update flashcard works

--- routes/FlashcardRouter.js
+++ routes/FlashcardRouter.js
@@ -1,10 +1,25 @@
 const Router = require('express').Router()
 const controller = require('../controllers/FlashcardController')
+const { Set, Flashcard, User } = require('../models')
 
 
+
+Router.post('/:set_id/create', async (req, res) => {
+    try {
+        let setId = parseInt(req.params.set_id)
+        let setBody = {
+          setId,
+          ...req.body
+        }
+        let flashcard = await Flashcard.create(setBody)
+        res.send(flashcard)
+      } catch (error) {
+        throw error
+      }
+})
 
 Router.get('/:flashcard_id/get', controller.GetFlashcardDetails)
-Router.post('/:set_id/create', controller.CreateFlashcard)
+
 Router.put('/:set_id/create/:flashcard_id/update', controller.UpdateFlashcard)
 Router.delete('/:set_id/create/:flashcard_id/delete', controller.DeleteFlashcard)
 // Implement these routes
